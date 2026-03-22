f6fa86edd8119d4dd0281e9e5ab05e2803372690
Pankajkumargupta <pankajguptasafidon@gmail.com>
2026-03-22 13:39:31 +0530

    make component inside component and how to use component as a function

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,18 @@ export default function Home() {
    const apple=()=>{
      setName("gupta")
     }
+    const InnerComp =()=>{
+    return(
+      <h1>this is inner component1</h1>
+    )}
   return (
   
     <div>
       <h1 className="bg-green-400 flex-grid">hello {name} </h1>
      <button onClick={()=>apple()}>Click me</button>
-      
+      {/* <InnerComp/> */} 
+      {InnerComp()} 
+      {/* HOW TO USE COMPONENT AS A FUNCTION */}
     </div>
   );
 }
