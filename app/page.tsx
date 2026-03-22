25c39626a3ae9a70097a5821853fd10e10e313cd
Pankajkumargupta <pankajguptasafidon@gmail.com>
2026-03-22 12:59:11 +0530

    use function with paramerter

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 
 'use client'
 export default function Home() {
+   const apple=(item)=>{
+      alert(item);
+    }
   return (
-    // <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    //   <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-    //     <h1 className="bg-green-400 flex-grid">home page</h1>
-    //   </main>
-    // </div>
+  
     <div>
       <h1 className="bg-green-400 flex-grid">home page</h1>
-     <button onClick={()=>alert("hello next hs ")}>Click me</button>
+     <button onClick={()=>apple("green")}>Click me</button>
       
     </div>
   );
