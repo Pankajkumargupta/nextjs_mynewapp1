4f81b77afdae4e616d714551611088761fc70ba5
Pankajkumargupta <pankajguptasafidon@gmail.com>
2026-04-02 19:41:37 +0530

    Condetion With Style

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,22 @@
 // interface abcc{
 //   name: string;
 // }
-import custom from './custom.module.css'
-import other1 from './other.module.css'
+import custom from "./custom.module.css";
+import other1 from "./other.module.css";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import outside from './style/outside.module.css';
+import outside from "./style/outside.module.css";
+import style from "./style.module.css";
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
   const navigate = (name) => {
     router.push(name);
   };
+  const [color, setcolor] = useState("red");
+
+  const {red}=style; 
   return (
     <div>
       <h1>This is home page</h1>
@@ -41,7 +46,8 @@ export default function Home() {
       </Link>
       {/* <button onClick={()=>router.push("/logon")} className="bg-blue-500">go to login page</button><br/>
      <button onClick={()=>router.push("/about")} className="bg-green-500">go to about page</button> */}
-      <br /><br />
+      <br />
+      <br />
       <hr />
       <br />
       <hr />
@@ -51,6 +57,28 @@ export default function Home() {
       <p className={custom.main}>this is paragraph</p>
       <p className={other1.main}>this is paragraph</p>
       <p className={outside.main}>outside folder</p>
+      <div>
+        <h1 className={color == "red" ? style.red : style.green}>
+          condetion with css
+        </h1>
+        <button onClick={() => setcolor("green")}>
+          update green color button
+        </button>
+
+        <h2 style={{ backgroundColor: color == "red" ? "red" : "green" }}>
+          heading 2
+        </h2>
+
+        <h3 id={style.orange}>heading 3</h3>
+
+{/* const {red}=style;    write this before return start */}
+        <h4 className={red}>dummy text</h4>
+        <h4 className={red}>dummy text</h4>
+        <h4 className={red}>dummy text</h4>
+        <h4 className={red}>dummy text</h4>
+
+
+      </div>
     </div>
   );
 }
