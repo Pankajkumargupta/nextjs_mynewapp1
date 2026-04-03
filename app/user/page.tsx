ee52a70484ff44e8d68baa00d2d6bde44fbc5d07
Pankajkumargupta <pankajguptasafidon@gmail.com>
2026-04-03 19:43:23 +0530

    generateMatadata for Dynimic Meta

---
+++ app/user/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>user page</h1>
+    </div>
+  );
+}
+export function generateMetadata() {
+  return {
+    title: "user page title",
+    description: "user page description"
+  };
+}
